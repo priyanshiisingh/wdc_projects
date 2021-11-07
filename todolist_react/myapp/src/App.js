d59689dde5b9849bd7c830713c6d9dc7fe027080
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-11-07 19:55:18 +0530

    icons and footer

--- todolist_react/myapp/src/App.js
+++ todolist_react/myapp/src/App.js
@@ -1,11 +1,15 @@
 // import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
+import Footer from "./layouts/Footer";
 
 const App = () => {
   return (
     <div className="App">
       {/* <TodoForm /> */}
       <TodoList />
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 };
--- todolist_react/myapp/src/layouts/Icons.js
+++ todolist_react/myapp/src/layouts/Icons.js
@@ -1,8 +1,8 @@
 function Icons() {
   return (
     <div className="icons">
-      <i className="fa fa-times"></i>
-      <i className="fa fa-pencil"></i>
+      <i className="fa fa-times" style={{ fontSize: "1em" }}></i>
+      <i className="fa fa-pencil" style={{ fontSize: "1em" }}></i>
     </div>
   );
 }
